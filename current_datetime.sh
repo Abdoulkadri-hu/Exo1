755bfd6bcca939e0e50b0289e46dc178dc461c46
Abdoulkadri-hu <bakoabdoulkadri@gmail.com>
2025-05-11 01:55:14 +0100

    current_datetime.sh

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-date +%Y/%m/%d%H:%M:%S #la commande date permet d'afficher la date et l’heure actuelle %Y pour l'année, %m pour le mois,%d pour le jour,%H pour l'heure,%M pour la minute,%S pour la seconde 
+date +%Y-%m-%d%H:%M:%S #la commande date permet d'afficher la date et l’heure actuelle %Y pour l'année, %m pour le mois,%d pour le jour,%H pour l'heure,%M pour la minute,%S pour la seconde 
